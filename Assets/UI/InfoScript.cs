34550c3e3045dc89251fb327f2ecdc4c98a90c0f
ahmetrft <deepyellow18@gmail.com>
2024-08-01 17:10:50 +0300

    Info Menu Script
    Her sahnenin başında çıkacak olan bilgilendirme ekranı için script hazırlandı

---
+++ Assets/UI/InfoScript.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro; // Eğer TextMeshPro kullanıyorsanız
+
+public class InfoBox : MonoBehaviour
+{
+    public TextMeshProUGUI infoText; // TextMeshPro kullanıyorsanız
+    // public Text infoText; // Text kullanıyorsanız
+    public string message;
+    public float typingSpeed = 0.05f;
+    public float displayTime = 5f;
+
+    private bool isDisplaying = false;
+
+    void Start()
+    {
+        StartCoroutine(TypeMessage());
+    }
+
+    void Update()
+    {
+        if (isDisplaying && (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) ||
+                             Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D) ||
+                             Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow) ||
+                             Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow) ||
+                             Input.GetKeyDown(KeyCode.Escape)))
+        {
+            HideInfoBox();
+        }
+    }
+
+    IEnumerator TypeMessage()
+    {
+        isDisplaying = true;
+        infoText.text = "";
+        foreach (char letter in message.ToCharArray())
+        {
+            infoText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+        yield return new WaitForSeconds(displayTime);
+        HideInfoBox();
+    }
+
+    void HideInfoBox()
+    {
+        isDisplaying = false;
+        gameObject.SetActive(false);
+    }
+}
